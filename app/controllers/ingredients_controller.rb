dfe6124e299f991640e7555d59fc5968336e3c92
Ashley Knight <ak@subcq.com>
2013-06-12 21:02:14 +1000

    Redirect on update

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -42,7 +42,7 @@ class IngredientsController < ApplicationController
   def update
     respond_to do |format|
       if @ingredient.update(ingredient_params)
-        format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
+        format.html { redirect_to ingredients_url, notice: 'Ingredient was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }

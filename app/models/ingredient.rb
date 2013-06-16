cfd1a15ea651a43341a8c0b9888503962a7cf070
Ashley Knight <ak@subcq.com>
2013-06-16 16:55:18 +1000

    Update belongs to and has many

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
+  belongs_to :meal
+  has_many :line_items
   
   CATEGORYTYPES = ['Sub Type', 'Bread', 'Sub', 'Cheese/Add on', 'Salad', 'Condiment']
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,4 @@
 class Meal < ActiveRecord::Base
+  has_many :line_items
   has_many :ingredients, through: :line_items
 end
